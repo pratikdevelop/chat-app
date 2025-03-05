310c2a9d20f328376b61f8e6e5310fe700d8f6e4
pratikdevelop <pratikraut88895@gmail.com>
2025-03-05 22:40:23 +0530

    upsdarte

---
+++ spring-chat-api/src/main/java/com/example/springchatapi/model/ExpireMe.java
@@ -0,0 +1,19 @@
+package com.example.springchatapi.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document(collection = "expireMe")
+public class ExpireMe {
+    @Id
+    private String id;
+    @Indexed(expireAfterSeconds = 360) // 6 minutes = 360 seconds
+    private Date createdAt = new Date();
+    private String userId;
+    private String otp;
+}
